eea7184cb6aaa2d295a1d5131ef36b897471ae18
Gustavo Henrique <gustavo@gustavohenrique.net>
2020-03-29 18:48:47 -0300

    Fix favicons and update json files

---
+++ site/src/components/TagsFilter/index.vue
@@ -0,0 +1,98 @@
+<template>
+  <q-card
+    flat
+    style="background:#efefef"
+  >
+    <q-card-section>
+      <q-btn
+        flat
+        dense
+        color="grey-8"
+        label="Filter by tags"
+        :icon-right="show ? 'expand_less' : 'expand_more'"
+        @click="show = !show"
+      />
+    </q-card-section>
+    <q-card-section
+      class="row q-gutter-md"
+      v-if="show"
+    >
+      <div v-if="!allTags || allTags.length === 0">No tags found.</div>
+      <q-chip
+        v-for="t in allTags"
+        text-color="white"
+        clickable
+        :key="t.id"
+        :color="getColorAccordingOf(t)"
+        @click="toggleSelectTag(t)"
+      >
+        {{ t.name }}
+      </q-chip>
+    </q-card-section>
+    <q-card-section
+      v-if="show"
+      class="row items-center"
+    >
+      <div class="text-grey">{{ allTags.length === 1 ? allTags.length + ' tag' : allTags.length + ' tags' }}</div>
+      <q-space />
+      <q-btn
+        flat
+        dense
+        no-caps
+        color="grey-8"
+        icon-right="expand_less"
+        @click="show = false"
+      />
+    </q-card-section>
+  </q-card>
+</template>
+
+<script>
+import constants from '../../constants'
+
+export default {
+  data () {
+    return {
+      show: false,
+      allTags: [],
+      selectedIds: []
+    }
+  },
+  created () {
+    this.readAll()
+    this.$subscribe(constants.TAGS_FILTER_REFRESH, this.readAll)
+  },
+  destroyed () {
+    this.$unsubscribe(constants.TAGS_FILTER_REFRESH)
+  },
+  watch: {
+    selectedIds: {
+      handler (val) {
+        this.$emit('updated', val)
+      },
+      deep: true
+    }
+  },
+  methods: {
+    async readAll () {
+      this.allTags = await this.$s.tag.readAll()
+    },
+    getColorAccordingOf (tag) {
+      if (this.isSelected(tag.id)) {
+        return 'negative'
+      }
+      return 'grey-6'
+    },
+    toggleSelectTag (tag) {
+      if (this.isSelected(tag.id)) {
+        this.selectedIds = this.selectedIds.filter(id => id !== tag.id)
+        return
+      }
+      this.selectedIds.push(tag.id)
+    },
+    isSelected (id) {
+      return this.selectedIds && this.selectedIds.some(i => i === id)
+    }
+  }
+}
+</script>
---
+++ site/src/pages/Repos/List.vue
@@ -0,0 +1,158 @@
+<template>
+  <q-page class="q-pa-lg">
+    <div class="text-h4 text-grey-8 q-mb-lg">
+      {{ pagination.rowsNumber === 1 ? pagination.rowsNumber + ' repository' : pagination.rowsNumber + ' repositories' }}
+    </div>
+
+    <div class="q-pt-lg q-pb-lg">
+      <tags-filter
+        @updated="filterByTags"
+      />
+    </div>
+
+    <div ref="cards">
+      <q-card
+        v-for="item in items"
+        :key="JSON.stringify(item)"
+        flat
+        bordered
+        class="col-2 q-mb-md"
+      >
+        <q-card-section>
+          <div class="row items-end">
+            <div>
+              <div class="text-headline text-grey">{{ formatDate(item.created_at) }}</div>
+              <div class="text-h5">{{ item.name }}</div>
+            </div>
+            <q-space />
+            <q-btn
+              flat
+              round
+              color="primary"
+              icon="open_in_new"
+              type="a"
+              target="__blank"
+              :href="item.url"
+            />
+          </div>
+          <div class="q-pt-md">
+            <div class="text-body2 text-primary">{{ item.description }}</div>
+          </div>
+        </q-card-section>
+
+        <q-separator v-if="item.tags && item.tags.length > 0" />
+
+        <q-card-section v-if="item.tags && item.tags.length > 0">
+          <q-chip
+            v-for="tag in item.tags"
+            :key="tag.id"
+            color="negative"
+            text-color="white"
+          >
+            {{ tag.name }}
+          </q-chip>
+        </q-card-section>
+      </q-card>
+
+    </div>
+
+    <div class="row justify-center q-my-md">
+      <q-btn
+        label="More"
+        color="primary"
+        @click="loadMore"
+        :loading="loading"
+        :disable="pagination.page >= pagination.maxPages"
+      />
+    </div>
+  </q-page>
+</template>
+
+<script>
+import { date } from 'quasar'
+
+export default {
+  data () {
+    return {
+      expandedId: '',
+      selected: {},
+      items: [],
+      loading: false,
+      tags: [],
+      pagination: {
+        page: 1,
+        maxPages: 0,
+        prevPage: 0,
+        rowsPerPage: 10,
+        rowsNumber: 10,
+        sortBy: 'created_at',
+        descending: true
+      }
+    }
+  },
+  mounted () {
+    this.search()
+  },
+  methods: {
+    isExpanded (id) {
+      return this.expandedId === id
+    },
+    toggleExpanded (id) {
+      if (this.isExpanded(id)) {
+        this.expandedId = ''
+      } else {
+        this.expandedId = id
+      }
+    },
+    formatDate (value) {
+      const dt = new Date(value)
+      return date.formatDate(dt, 'YYYY-MM-DD')
+    },
+    filterByTags (tags) {
+      this.items = []
+      this.tags = tags
+      this.search()
+    },
+    async loadMore () {
+      const { pagination } = this
+      if (pagination.page < pagination.maxPages) {
+        pagination.page += 1
+        await this.search({ pagination })
+      }
+    },
+    async search (params) {
+      this.loading = true
+      try {
+        const pagination = params && params.pagination ? params.pagination : this.pagination
+        let data = []
+        if (this.tags && this.tags.length > 0) {
+          this.tags.forEach(async tagId => {
+            const result = await this.$s.repo.searchByTag(tagId)
+            const unique = []
+            result.items.map(i => {
+              const alreadyAdded = this.items.some(added => added.id === i.id)
+              if (!alreadyAdded) {
+                unique.push(i)
+              }
+            })
+            this.items = this.items.concat(unique)
+            this.pagination.rowsNumber = this.items.length
+          })
+        } else {
+          data = await this.$s.repo.search({ pagination })
+          this.items = this.items.concat(data.items)
+        }
+        const maxPages = Math.ceil(data.total / 10)
+        this.pagination = {
+          ...pagination,
+          maxPages,
+          rowsNumber: data.total
+        }
+      } catch (err) {
+        this.$s.dialog.error(err)
+      }
+      this.loading = false
+    }
+  }
+}
+</script>
